b22a8d2d4f333c3884d8708a90d2b6285aee3f0d
Ayangbile Daniel Bayo <65937766+DanielBayo@users.noreply.github.com>
2023-02-18 20:51:38 +0000

    Update Market Basket Analysis.sql

--- Market Basket Analysis.sql
+++ Market Basket Analysis.sql
@@ -1,27 +1,23 @@
-USE AdventureWorks2019
-GO
+USE adventureworks2019
 
-WITH order_item AS (
-SELECT DISTINCT
-	a.CustomerKey
-	,a.OrderDateKey
-	,a.ProductKey
-	,b.EnglishProductName as ProductName
-FROM [AdventureWorksDW2019].[dbo].[FactInternetSales] a
-INNER JOIN [AdventureWorksDW2019].[dbo].[DimProduct] b 
-ON a.ProductKey = b.ProductKey
-)
+go
 
-SELECT
-	c.ProductName
-	,d.ProductName
-	,COUNT(*) frequency
-FROM order_item c
-INNER JOIN order_item d
-ON c.CustomerKey = d.CustomerKey
-AND c.OrderDateKey = d.OrderDateKey
-WHERE 	c.ProductName < d.ProductName
-GROUP BY 
-	c.ProductName
-	,d.ProductName
-ORDER BY COUNT(*) DESC
+WITH order_item
+     AS (SELECT DISTINCT a.customerkey,
+                         a.orderdatekey,
+                         a.productkey,
+                         b.englishproductname AS ProductName
+         FROM   [AdventureWorksDW2019].[dbo].[factinternetsales] a
+                INNER JOIN [AdventureWorksDW2019].[dbo].[dimproduct] b
+                        ON a.productkey = b.productkey)
+SELECT c.productname,
+       d.productname,
+       Count(*) frequency
+FROM   order_item c
+       INNER JOIN order_item d
+               ON c.customerkey = d.customerkey
+                  AND c.orderdatekey = d.orderdatekey
+WHERE  c.productname < d.productname
+GROUP  BY c.productname,
+          d.productname
+ORDER  BY Count(*) DESC
